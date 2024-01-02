a70907a2fc4c8a30962fa6e7660328a6f3bba34a
CristianoSantan <cristiano.mer@gmail.com>
2024-01-02 18:00:59 -0300

    first commit

---
+++ api/booki_api/booki_api/Context/DataContext.cs
@@ -0,0 +1,13 @@
+﻿
+using booki_api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace booki_api.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Usuarios> Usuarios { get; set; }
+    }
+}
---
+++ api/booki_api/booki_api/Models/Usuarios.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace booki_api.Models
+{
+    public class Usuarios
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nome é obrigatorio")]
+        [MaxLength(60, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres")]
+        [MinLength(3, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres")]
+        public string nome { get; set; }
+        public string email { get; set; }
+        public string senha { get; set; }
+
+    }
+}
